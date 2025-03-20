c1e8679f1c20140ecddece0d4180b18c0c6ecbb7
leopn <omidpnteaching@gmail.com>
2025-03-20 14:18:28 -0400

    blade introduction

---
+++ task-list/resources/views/index.blade.php
@@ -0,0 +1,45 @@
+Hello I am from Blade Template
+{{-- @isset($name)
+    <div>
+        My Name is : {{$name}}
+    </div>
+@endisset --}}
+
+
+{{-- <div>
+    @if (count($tasks))
+    <h2> Title</h2>
+    @foreach ($tasks as $task)
+        <div>{{$task->title}}</div>
+    @endforeach
+    <div> There are tasks!</div>
+    @else
+    <div> No tasks!</div>
+    @endif
+</div> --}}
+
+{{-- <h1>The list of tasks: </h1>
+
+<div>
+@forelse ($tasks as $task)
+    <li>{{$task->title}}</li>
+@empty
+  <div> Tasks list is empty</div>
+@endforelse
+
+</div> --}}
+
+
+<h1>The list of tasks: </h1>
+
+<div>
+@forelse ($tasks as $task)
+    <li>
+        <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{$task->title}}
+    </li>
+@empty
+  <div> Tasks list is empty</div>
+@endforelse
+
+</div>
+
