a52dbf21b57268fac2de3198fd87af9d2e817234
leopn <omidpnteaching@gmail.com>
2025-03-24 03:24:27 -0400

    Reading data

--- task-list/routes/web.php
+++ task-list/routes/web.php
@@ -2,79 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 
-class Task
-{
-  public function __construct(
-    public int $id,
-    public string $title,
-    public string $description,
-    public ?string $long_description,
-    public bool $completed,
-    public string $created_at,
-    public string $updated_at
-  ) {
-  }
-}
-
-$tasks = [
-  new Task(
-    1,
-    'Buy groceries',
-    'Task 1 description',
-    'Task 1 long description',
-    false,
-    '2025-03-01 12:00:00',
-    '2025-03-01 12:00:00'
-  ),
-  new Task(
-    2,
-    'Sell old stuff',
-    'Task 2 description',
-    null,
-    false,
-    '2025-03-02 12:00:00',
-    '2025-03-02 12:00:00'
-  ),
-  new Task(
-    3,
-    'Learn programming',
-    'Task 3 description',
-    'Task 3 long description',
-    true,
-    '2025-03-03 12:00:00',
-    '2025-03-03 12:00:00'
-  ),
-  new Task(
-    4,
-    'Take dogs for a walk',
-    'Task 4 description',
-    null,
-    false,
-    '2025-03-04 12:00:00',
-    '2025-03-04 12:00:00'
-  ),
-];
 Route::get('/', function () {
     return redirect()->route('tasks.index');
 
 });
 
 // to make external call you need use($variable) after annynomous function
-Route::get('/tasks', function () use ($tasks) {
+Route::get('/tasks', function () {
     return view('index',[
-        'tasks'=>$tasks
+        // 'tasks'=>App\Models\Task::all()
+        'tasks'=>App\Models\Task::latest()->get()
+        // 'tasks'=>App\Models\Task::latest()->where('completed',true)->get()
     ]);
 })->name('tasks.index');
 
 // this is the route for every task
-Route::get('/{id}',function($id)use($tasks){
+Route::get('/tasks/{id}',function($id){
+    // App\Models\Task::find($id);
 
-    // arrays are not objects in php like ArrayList in Java , so we use collect to make use of collection in php
-    $task=collect($tasks)->firstWhere('id',$id);
-    if(!$task){
-    abort(Response::HTTP_NOT_FOUND);
-    }
     return view('show',[
-        'task'=>$task
-        ]);
+        // 'task'=>App\Models\Task::find($id)]);
+        'task'=>App\Models\Task::findOrFail($id)]);
     })->name('tasks.show');
