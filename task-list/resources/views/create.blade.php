6e98b9ceb212c771654571220f69eaae5e581e90
leopn <omidpnteaching@gmail.com>
2025-03-24 09:30:14 -0400

    01-forms

---
+++ task-list/resources/views/create.blade.php
@@ -0,0 +1,1 @@
+<h1>This is the create view</h1>
--- task-list/routes/web.php
+++ task-list/routes/web.php
@@ -10,17 +10,19 @@ Route::get('/', function () {
 // to make external call you need use($variable) after annynomous function
 Route::get('/tasks', function () {
     return view('index',[
-        // 'tasks'=>App\Models\Task::all()
+
         'tasks'=>App\Models\Task::latest()->get()
-        // 'tasks'=>App\Models\Task::latest()->where('completed',true)->get()
+
     ]);
 })->name('tasks.index');
+// Don't forget the order of routes ar important when path is the same
+Route::view('/tasks/create','create');
 
-// this is the route for every task
 Route::get('/tasks/{id}',function($id){
-    // App\Models\Task::find($id);
+
 
     return view('show',[
-        // 'task'=>App\Models\Task::find($id)]);
+
         'task'=>App\Models\Task::findOrFail($id)]);
     })->name('tasks.show');
+
