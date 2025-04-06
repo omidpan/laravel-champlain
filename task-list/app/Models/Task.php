3f87b925200ccbd862670b2d3133628f877c5819
leopn <omidpnteaching@gmail.com>
2025-04-06 13:54:42 -0400

    mdoel-binding-init

--- task-list/app/Models/Task.php
+++ task-list/app/Models/Task.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Task extends Model
 {
     use HasFactory;
+
+    // public function getRouteKeyName()
+    // {
+    //     return "slug";
+    //      // slug comming from string class.
+    //     // It converts a given string into a URL-friendly format by replacing spaces and special characters
+    //     //with hyphens and removing any non-ASCII characters
+    // }
+
+
 }
--- task-list/routes/web.php
+++ task-list/routes/web.php
@@ -19,16 +19,16 @@ Route::get('/tasks', function () {
 // Don't forget the order of routes ar important when path is the same
 Route::view('/tasks/create','create')->name('tasks.create');
 
-Route::get('/tasks/{id}/edit',function($id){
-    return view('eidt',['task'=>Task::findOrFail($id)]);
+Route::get('/tasks/{task}/edit',function(Task $task) {
+    return view('eidt',['task'=> $task]);
     })->name('tasks.edit');
 
-Route::get('/tasks/{id}',function($id){
-    return view('show',['task'=>Task::findOrFail($id)]);
+Route::get('/tasks/{task}',function(Task $task) {
+    return view('show',['task'=>$task]);
     })->name('tasks.show');
 
-Route::get('/tasks/{id}/edit',function($id){
-        return view('edit',['task'=>Task::findOrFail($id)]);
+Route::get('/tasks/{task}/edit',function(Task $task) {
+        return view('edit',['task'=> $task]);
         })->name('tasks.edit');
 
 Route::post('/tasks',function(Request $request){
@@ -46,24 +46,23 @@ Route::post('/tasks',function(Request $request){
     // Laravel model is smart enough to save data in the database with save() method.
     $task->save();
 // let's redirect to the already created task
-    return redirect()->route('tasks.show', ['id' => $task->id])->with('success','Task created successfully');
+    return redirect()->route('tasks.show', ['task' => $task->id])->with('success','Task created successfully');
 })->name('tasks.store');
 
 
 //Update form
-Route::put('/tasks/{id}', function ($id, Request $request) {
+Route::put('/tasks/{task}', function (Task $task, Request $request) {
     $data = $request->validate([
         'title' => 'required|max:255',
         'description' => 'required',
         'long_description' => 'required'
     ]);
 
-    $task = Task::findOrFail($id);
     $task->title = $data['title'];
     $task->description = $data['description'];
     $task->long_description = $data['long_description'];
     $task->save();
 
-    return redirect()->route('tasks.show', ['id' => $task->id])
+    return redirect()->route('tasks.show', ['task' => $task->id])
         ->with('success', 'Task updated successfully!');
 })->name('tasks.update');
