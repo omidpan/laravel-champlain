e940e11a18a87fb59d84a04b7091be8a41dc4b64
leopn <omidpnteaching@gmail.com>
2025-04-06 22:42:46 -0400

    add toggle task completed or not completed

--- task-list/resources/views/index.blade.php
+++ task-list/resources/views/index.blade.php
@@ -1,18 +1,23 @@
-<h1>The list of tasks: </h1>
+@extends('app')
 
-<div>
-    @forelse ($tasks as $task)
-        <li>
-            <a href="{{ route('tasks.show', ['task' => $task->id]) }}">{{$task->title}}
-        </li>
-    @empty
-    <div> Tasks list is empty</div>
-    @endforelse
-    <div></div>
-    @if ($tasks->count())
-        <nav>
-            {{$tasks->links()}}
-        </nav>
-    @endif
+ @section('title', 'The list of tasks')
 
-</div>
+ @section('content')
+   <div>
+     <a href="{{ route('tasks.create') }}">Add Task!</a>
+   </div>
+
+   @forelse ($tasks as $task)
+     <div>
+       <a href="{{ route('tasks.show', ['task' => $task->id]) }}">{{ $task->title }}</a>
+     </div>
+   @empty
+     <div>There are no tasks!</div>
+   @endforelse
+
+   @if ($tasks->count())
+     <nav>
+       {{ $tasks->links() }}
+     </nav>
+   @endif
+ @endsection
--- task-list/routes/web.php
+++ task-list/routes/web.php
@@ -1,49 +1,70 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Models\Task;
-use App\Http\Requests\TaskRequest;
-Route::get('/', function () {
-    return redirect()->route('tasks.index');
+ use App\Http\Requests\TaskRequest;
+ use App\Models\Task;
+ use Illuminate\Http\Response;
+ use Illuminate\Http\Request;
+ use Illuminate\Support\Facades\Route;
 
-});
+ /*
+ |--------------------------------------------------------------------------
+ | Web Routes
+ |--------------------------------------------------------------------------
+ |
+ | Here is where you can register web routes for your application. These
+ | routes are loaded by the RouteServiceProvider and all of them will
+ | be assigned to the "web" middleware group. Make something great!
+ |
+ */
 
-Route::get('/tasks', function () {
-    return view('index',['tasks'=>Task::latest()->paginate()]);
-})->name('tasks.index');
+ Route::get('/', function () {
+     return redirect()->route('tasks.index');
+ });
 
+ Route::get('/tasks', function () {
+     return view('index', [
+         'tasks' => Task::latest()->paginate(10)
+     ]);
+ })->name('tasks.index');
 
-Route::view('/tasks/create','create')->name('tasks.create');
+ Route::view('/tasks/create', 'create')
+     ->name('tasks.create');
 
-Route::get('/tasks/{task}/edit',function(Task $task) {
-    return view('eidt',['task'=> $task]);
-    })->name('tasks.edit');
+ Route::get('/tasks/{task}/edit', function (Task $task) {
+     return view('edit', [
+         'task' => $task
+     ]);
+ })->name('tasks.edit');
 
-Route::get('/tasks/{task}',function(Task $task) {
-    return view('show',['task'=>$task]);
-    })->name('tasks.show');
+ Route::get('/tasks/{task}', function (Task $task) {
+     return view('show', [
+         'task' => $task
+     ]);
+ })->name('tasks.show');
 
-Route::get('/tasks/{task}/edit',function(Task $task) {
-        return view('edit',['task'=> $task]);
-        })->name('tasks.edit');
+ Route::post('/tasks', function (TaskRequest $request) {
+     $task = Task::create($request->validated());
 
-Route::post('/tasks',function(TaskRequest $request){
-    //with using taskRequest validated() method
-    $data = $request->validated();
-    $task = Task::create($data);
-    return redirect()->route('tasks.show', ['task' => $task->id])->with('success','Task created successfully');
-})->name('tasks.store');
+     return redirect()->route('tasks.show', ['task' => $task->id])
+         ->with('success', 'Task created successfully!');
+ })->name('tasks.store');
 
+ Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
+     $task->update($request->validated());
 
-//Update form
-Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
-   $task->update($request->validated());
+     return redirect()->route('tasks.show', ['task' => $task->id])
+         ->with('success', 'Task updated successfully!');
+ })->name('tasks.update');
 
-    return redirect()->route('tasks.show', ['task' => $task->id])
-        ->with('success', 'Task updated successfully!');
-})->name('tasks.update');
+ Route::delete('/tasks/{task}', function (Task $task) {
+     $task->delete();
 
-Route::delete('/tasks/{task}',function(Task $task) {
-        $task->delete();
-        return redirect()->route('tasks.index')->with('success','Task deleted successfully');
-})->name('tasks.destroy');
+     return redirect()->route('tasks.index')
+         ->with('success', 'Task deleted successfully!');
+ })->name('tasks.destroy');
+
+ Route::put('tasks/{task}/toggle-complete', function (Task $task) {
+     $task->toggleComplete();
+
+     return redirect()->back()->with('success', 'Task updated successfully!');
+ })->name('tasks.toggle-complete');
