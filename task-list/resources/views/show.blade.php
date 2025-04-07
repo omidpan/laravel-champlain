278ae6f7a0070bd190ea6c4a904a2c0c8449fef2
leopn <omidpnteaching@gmail.com>
2025-04-06 20:41:48 -0400

    delete route

--- task-list/resources/views/show.blade.php
+++ task-list/resources/views/show.blade.php
@@ -14,4 +14,13 @@
     <p>{{$task->created_at}}</p>
     <p>{{$task->updated_at}}</p>
 
+    <div>
+            <form action="{{  route('tasks.destroy',['task'=>$task->id])  }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button type="submit"> Remove Task </button>
+            </form>
+
+    </div>
+
 @endsection
