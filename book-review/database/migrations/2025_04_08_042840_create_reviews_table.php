18a69c9b27b20072ea3d047089691619fa1b582a
leopn <omidpnteaching@gmail.com>
2025-04-09 23:11:36 -0400

    add columns to migration script

--- book-review/database/migrations/2025_04_08_042840_create_reviews_table.php
+++ book-review/database/migrations/2025_04_08_042840_create_reviews_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            $table->text('review');
+            $table->unsignedBigInteger('rating');
+
             $table->timestamps();
         });
     }
