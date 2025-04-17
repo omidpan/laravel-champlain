963b87ebdc340e4ae3cb048b2667acaa608a6d02
leopn <omidpnteaching@gmail.com>
2025-04-17 13:33:27 -0400

    Wire route with Resource controller

--- book-review/routes/web.php
+++ book-review/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('Books',BookController::class);//automatic route
