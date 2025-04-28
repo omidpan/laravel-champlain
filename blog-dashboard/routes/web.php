28540f0ec414f00c4102d638d08981dce9cb4c04
leopn <omidpnteaching@gmail.com>
2025-04-28 09:02:50 -0400

    add user-admin views and routes

--- blog-dashboard/routes/web.php
+++ blog-dashboard/routes/web.php
@@ -2,10 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
+// import Auth
+use Illuminate\Support\Facades\Auth;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 Auth::routes();
 
+Route::get('/admin', function () {
+    return view('admin');
+})->middleware('auth', 'role:admin');
+
+Route::get('/user', function () {
+    return view('user');
+})->middleware('auth', 'role:user');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
